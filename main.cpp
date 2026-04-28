ac815412b91116fe0fb4f7adacab4de65cc2565c
YXZL985 <cym666xx@163.com>
2026-04-28 12:31:34 +0800

    Switch to DTK6 + Qt6 stack with empty window
    - Update CMakeLists.txt to use DTK6 (required)
    - Remove DApplicationSettings (not available in DTK6)
    - Clear window content, keep only empty layout
    - Link with Dtk6::Widget, Dtk6::Core, Dtk6::Gui

--- main.cpp
+++ main.cpp
@@ -1,64 +1,39 @@
 #include <DApplication>
 #include <DMainWindow>
 #include <DWidgetUtil>
-#include <DApplicationSettings>
 #include <DTitlebar>
-#include <DProgressBar>
-#include <DFontSizeManager>
 
-#include <QPropertyAnimation>
-#include <QDate>
-#include <QLayout>
+#include <QVBoxLayout>
+#include <QWidget>
 
 DWIDGET_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
-    // Qt6 中 AA_UseHighDpiPixmaps 已被移除，高DPI默认启用
     DApplication a(argc, argv);
     a.setOrganizationName("deepin");
-    a.setApplicationName("dtk-application");
+    a.setApplicationName("candle");
     a.setApplicationVersion("1.0");
     a.setProductIcon(QIcon(":/images/logo.svg"));
-    a.setProductName("Dtk Application");
-    a.setApplicationDescription("This is a dtk template application.");
+    a.setProductName("Candle");
+    a.setApplicationDescription("A simple DTK6 application.");
 
     a.loadTranslator();
-    a.setApplicationDisplayName(QCoreApplication::translate("Main", "DTK Application"));
-
-    // 保存程序的窗口主题设置
-    DApplicationSettings as;
-    Q_UNUSED(as)
+    a.setApplicationDisplayName(QCoreApplication::translate("Main", "Candle"));
 
     DMainWindow w;
     w.titlebar()->setIcon(QIcon(":/images/logo.svg"));
-    w.titlebar()->setTitle("Hello dtk");
-    // 设置标题，宽度不够会隐藏标题文字
-    w.setMinimumSize(QSize(600, 200));
+    w.titlebar()->setTitle("Candle");
+    w.setMinimumSize(QSize(600, 400));
 
+    // 创建空窗体
     QWidget *cw = new QWidget(&w);
     QVBoxLayout *layout = new QVBoxLayout(cw);
-    QDate today = QDate::currentDate();
-    DProgressBar *yearProgressBar = new DProgressBar();
-    yearProgressBar->setMaximum(today.daysInYear());
-    // 绑定字体大小
-    DFontSizeManager::instance()->bind(yearProgressBar, DFontSizeManager::T1);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
 
-    yearProgressBar->setAlignment(Qt::AlignCenter);
-    QObject::connect(yearProgressBar, &DProgressBar::valueChanged, yearProgressBar, [yearProgressBar](int value){
-        yearProgressBar->setFormat(QString("您的 %1 使用进度： %2%").arg(QDate::currentDate().year())
-                                   .arg(value * 100 / yearProgressBar->maximum()));
-    });
-
-    layout->addWidget(yearProgressBar);
     w.setCentralWidget(cw);
     w.show();
-
-    auto animation = new QPropertyAnimation(yearProgressBar, "value");
-    animation->setDuration(5000);
-    animation->setStartValue(0);
-    animation->setEndValue(today.dayOfYear());
-    animation->start();
 
     Dtk::Widget::moveToCenter(&w);
 
